d8550d3dc93c0baf3c8cf6c109aa829956ad1a32
siddartha.bandi <siddartha.bandi@infanion.com>
2024-11-26 19:04:46 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        siddu
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -47,7 +48,8 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
+        heloo world am siddu
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
